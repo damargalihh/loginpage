c182ab00c8bda8d8894075d5af5a2f584836fd74
damargalihh <l200230184@student.ums.ac.id>
2026-02-03 11:32:39 +0700

    mpas

--- contact.php
+++ contact.php
@@ -99,7 +99,7 @@ $prefill_nim = isset($_SESSION['nim']) ? $_SESSION['nim'] : '';
                     <!-- Left: Map -->
                     <div style="border-radius: 12px; overflow: hidden; min-height: 300px;">
                         <iframe 
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1977.5!2d110.8156!3d-7.5389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a144b5c5d6e7f%3A0x1234567890abcdef!2sJl.%20Tulang%20Bawang%20Sel.%20No.26%2C%20Kadipiro%2C%20Banjarsari%2C%20Surakarta!5e0!3m2!1sen!2sid!4v1699000000000!5m2!1sen!2sid" 
+                            src="https://maps.google.com/maps?q=-7.5389,110.8156&z=17&output=embed" 
                             width="100%" 
                             height="100%" 
                             style="border:0; min-height: 300px;" 
